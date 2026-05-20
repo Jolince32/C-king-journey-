cc31aa38a845a8413ae7f077c86186eb59cef4b4
Jolince32 <jolinceking@gmail.com>
2026-05-20 23:37:26 +0530

    Create Calculator_v1.c

---
+++ Calculator_v1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    float num1, num2;
+    printf("num 1: ");
+    scanf("%f", &num1);
+    printf("num 2: ");
+    scanf("%f", &num2);
+    printf("answer = %.2f \n", num1 + num2);
+    return 0;
+}
